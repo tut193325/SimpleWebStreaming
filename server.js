2e22f0d43d7f3c6b614647b423c042b9341cbd53
Shank <kobayashi.yugo.vv@tut.jp>
2024-01-16 18:04:47 +0900

    delete indivual

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const url = require('url');
 const htmlData = fs.readFileSync('./simple.html', 'UTF-8');
 const cssData =  fs.readFileSync('./simple.css', 'UTF-8');
 const jsData =  fs.readFileSync('./simple.js', 'UTF-8');
-const jpegData = fs.readFileSync('./umemura.jpg');
+// const jpegData = fs.readFileSync('./umemura.jpg');
 const videoPath = './20180310-rehearsal.mp4';
 
 // Note that this service responds 'readme.html' (not 'index.html') for  '/'.
@@ -41,45 +41,45 @@ function serviceClient(request, response) {
 	response.write(jsData);
 	response.end(); 
 	break;
-    case '/umemura.jpg':
-	response.writeHead(
-			   200, 
-			   {'Content-Type' : 'image/jpeg', 
-			    'Content-Length' : jpegData.length,
-			   });
-	response.write(jpegData);
-	response.end(); 
-	break;
-    case '/20180310-rehearsal.mp4':
-	// reference: https://medium.com/@daspinola/video-stream-with-node-js-and-html5
-	// Note that video file may be very large file. 
-	const path = videoPath;
-	const stat = fs.statSync(path);
-	const fileSize = stat.size;
-	const range = request.headers.range;
-	if (range) {
-	  const parts = range.replace(/bytes=/, "").split("-");
-	  const start = parseInt(parts[0], 10);
-	  const end = parts[1] ? parseInt(parts[1], 10) : fileSize-1;
-	  const chunksize = (end-start)+1;
-	  const file = fs.createReadStream(path, {start, end});
-	  const head = {
-	      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-	      'Accept-Ranges': 'bytes',
-	      'Content-Length': chunksize,
-	      'Content-Type': 'video/mp4',
-	  }
-	  response.writeHead(206, head);
-	  file.pipe(response);
-	} else {
-	  const head = {
-	      'Content-Length': fileSize,
-	      'Content-Type': 'video/mp4',
-	  }
-	  response.writeHead(200, head);
-	  fs.createReadStream(path).pipe(response);
-	}
-	break;
+    // case '/umemura.jpg':
+	// response.writeHead(
+	// 		   200, 
+	// 		   {'Content-Type' : 'image/jpeg', 
+	// 		    'Content-Length' : jpegData.length,
+	// 		   });
+	// response.write(jpegData);
+	// response.end(); 
+	// break;
+    // case '/20180310-rehearsal.mp4':
+	// // reference: https://medium.com/@daspinola/video-stream-with-node-js-and-html5
+	// // Note that video file may be very large file. 
+	// const path = videoPath;
+	// const stat = fs.statSync(path);
+	// const fileSize = stat.size;
+	// const range = request.headers.range;
+	// if (range) {
+	//   const parts = range.replace(/bytes=/, "").split("-");
+	//   const start = parseInt(parts[0], 10);
+	//   const end = parts[1] ? parseInt(parts[1], 10) : fileSize-1;
+	//   const chunksize = (end-start)+1;
+	//   const file = fs.createReadStream(path, {start, end});
+	//   const head = {
+	//       'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+	//       'Accept-Ranges': 'bytes',
+	//       'Content-Length': chunksize,
+	//       'Content-Type': 'video/mp4',
+	//   }
+	//   response.writeHead(206, head);
+	//   file.pipe(response);
+	// } else {
+	//   const head = {
+	//       'Content-Length': fileSize,
+	//       'Content-Type': 'video/mp4',
+	//   }
+	//   response.writeHead(200, head);
+	//   fs.createReadStream(path).pipe(response);
+	// }
+	// break;
 
 
     default:
